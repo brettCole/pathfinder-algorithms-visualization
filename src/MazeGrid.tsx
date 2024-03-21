a2b7e99a18dd75880c4df6ea0243874edd80de34
brett.cole <acodercalledcole@gmail.com>
2024-03-21 11:05:36 -0400

    initial generateMaze function

--- src/MazeGrid.tsx
+++ src/MazeGrid.tsx
@@ -1,17 +1,45 @@
+import { useState } from 'react';
 import './MazeGrid.css'
 
 function MazeGrid() {
-
-  let maze = [
+  let initialMaze = [
     ['wall', 'wall', 'wall', 'wall'],
     ['start', 'path', 'path', 'wall'],
     ['wall', 'wall', 'path', 'end'],
     ['wall', 'wall', 'wall', 'wall']
   ];
 
+  const [maze, setMaze] = useState(initialMaze);
+
+  function generateMaze(height, width) {
+    const matrix = [];
+
+    for (let i = 0; i < height; i++) {
+      const row = [];
+      for (let j = 0; j < width; j++) {
+        const cell = Math.random()
+        if (cell < 0.5) {
+          row.push('wall')
+        } else {
+          row.push('path')
+        }
+      }
+      matrix.push(row);
+    }
+    console.log(matrix);
+    matrix[1][0] = 'start';
+    matrix[height - 2][width - 1] = 'end';
+    setMaze(matrix);
+  }
+
   return (
     <div className='maze-grid'>
-      <button className='maze-button'>Refresh Maze</button>
+      <button 
+        className='maze-button' 
+        onClick={() => generateMaze(5, 6)}
+      >
+        Refresh Maze
+      </button>
       <div className="maze">
         {maze.map((row, rowIndex) => (
           <div className='row'>
