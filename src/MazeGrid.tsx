0410e24b04cdf9550fd96301983be740fed4baad
brett.cole <acodercalledcole@gmail.com>
2024-03-21 10:49:56 -0400

    start of maze layout and associated css

--- src/MazeGrid.tsx
+++ src/MazeGrid.tsx
@@ -1,13 +1,29 @@
-import './App.css'
+import './MazeGrid.css'
 
 function MazeGrid() {
 
+  let maze = [
+    ['wall', 'wall', 'wall', 'wall'],
+    ['start', 'path', 'path', 'wall'],
+    ['wall', 'wall', 'path', 'end'],
+    ['wall', 'wall', 'wall', 'wall']
+  ];
+
   return (
-    <div>
-      <div className={'cell wall'}></div>
-      <div className={'cell path'}></div>
-      <div className={'cell start'}></div>
-      <div className={'cell end'}></div>
+    <div className='maze-grid'>
+      <button className='maze-button'>Refresh Maze</button>
+      <div className="maze">
+        {maze.map((row, rowIndex) => (
+          <div className='row'>
+            {row.map((cell, cellIndex) => (
+              <div className={`cell ${cell}`}>
+
+              </div>
+            ))}
+          </div>
+        ))}
+        
+      </div>
     </div>
   )
 }
